afca366fd5c3674372c291308bcaa00a4eca663a
Aaron-Ccente <aronccente@gmail.com>
2025-08-18 15:58:02 -0500

    Conflictos resueltos y uniendo el commit de App con lo actual

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-<<<<<<< HEAD
 import { server } from './src/app.js';
 import { AUTH_CONFIG } from './src/config/auth.config.js';
 
@@ -9,11 +8,5 @@ const PORT = process.env.PORT || 5111;
 AUTH_CONFIG.validateConfig();
 
 server.listen(PORT, () => {
-=======
-import app from './src/app.js';
-
-const PORT = process.env.PORT || 5111;
-app.listen(PORT, () => {
->>>>>>> b730d8d (App)
   console.log(`Servidor WhatsApp corriendo en puerto ${PORT}`);
 });
