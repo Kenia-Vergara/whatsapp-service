da6d5d3d84a834410f762e8ee2fdb926346c15de
Aaron-Ccente <aronccente@gmail.com>
2025-08-18 15:52:50 -0500

    Conflictos resueltos

--- src/validators/message.validator.js
+++ src/validators/message.validator.js
@@ -1,22 +1,9 @@
 import { body, validationResult } from 'express-validator';
 
-const validTemplates = [
-  'cita_gratis',
-  'cita_pagada',
-  'recordatorio_cita',
-  'confirmacion_asistencia'
-];
-
 export const validateSendMessage = [
   body('phone').isString().notEmpty().withMessage('Teléfono requerido'),
-  body('templateOption')
-    .isString()
-    .notEmpty().withMessage('Plantilla requerida')
-    .isIn(validTemplates).withMessage('Plantilla no válida'),
+  body('templateOption').isString().notEmpty().withMessage('Plantilla requerida'),
   // Puedes agregar más validaciones según tus necesidades
-  body('psicologo').isString().notEmpty().withMessage('Psicologo requerido'),
-  body('fecha').isString().notEmpty().withMessage('Fecha requerida'),
-  body('hora').isString().notEmpty().withMessage('Hora requerida'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
