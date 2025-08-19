b25e3bfd7308a154ecb817f288030282af458bb1
Aaron-Ccente <aronccente@gmail.com>
2025-08-18 21:26:34 -0500

    Para usar los mensajes al whatsapp desde el frontend

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,4 +1,3 @@
-import { Router } from 'express';
 import {
   sendMessage,
   getStatus,
@@ -6,20 +5,28 @@ import {
   forceExpireQr,
   getQrStatus,
   getQrCode,
-  resetAuth
+  resetAuth,
+  getSentMessages,
+  checkAuthStatus,
+  forceReconnect,
+  getReconnectionStatus
 } from '../controllers/message.controller.js';
 import { validateSendMessage } from '../validators/message.validator.js';
 import { authenticateJWT, authorizeRole } from '../middlewares/auth.middleware.js';
+import { Router } from 'express';
 
 const router = Router();
 
 router.post('/send-message', authenticateJWT, authorizeRole('admin'), validateSendMessage, sendMessage);
+router.get('/sent-messages', authenticateJWT, authorizeRole('admin'), getSentMessages);
 router.get('/qr-code', authenticateJWT, authorizeRole('admin'), getQrCode);
 router.get('/status', authenticateJWT, getStatus);
 router.get('/qr-status', authenticateJWT, getQrStatus);
+router.get('/auth-status', authenticateJWT, checkAuthStatus);
+router.get('/reconnection-status', authenticateJWT, getReconnectionStatus);
 router.post('/qr-request', authenticateJWT, authorizeRole('admin'), requestNewQr);
 router.post('/qr-expire', authenticateJWT, authorizeRole('admin'), forceExpireQr);
 router.post('/auth/reset', authenticateJWT, authorizeRole('admin'), resetAuth);
-
+router.post('/force-reconnect', authenticateJWT, authorizeRole('admin'), forceReconnect);
 
 export default router;
