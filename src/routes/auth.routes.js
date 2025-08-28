a0f205961d8c5ed8f88af7d4e14f8b8df66c3bc7
Aaron-Ccente <aronccente@gmail.com>
2025-08-27 19:14:53 -0500

    Ruta para validar token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, getCurrentUser } from '../controllers/auth.controller.js';
+import { login, getCurrentUser, validateToken } from '../controllers/auth.controller.js';
 import { validateLogin } from '../validators/auth.validator.js';
 import { authenticateJWT } from '../middlewares/auth.middleware.js';
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post('/login', validateLogin, login);
 router.get('/me', authenticateJWT, getCurrentUser);
+router.get('/validate', authenticateJWT, validateToken);
 
 export default router;
